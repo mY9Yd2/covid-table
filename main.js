b38b569df74e4f53eb72916101750ddde5e88a94
Kovács József Miklós <kovacsjozsef7u@gmail.com>
2021-07-28 15:20:57 +0200

    refactor

--- main.js
+++ main.js
@@ -20,63 +20,70 @@ function escapeHTML(unsafe) {
         .replace(/'/g, '&#039;');
 }
 
+function addDataToTable(tableData) {
+    for (let i = 0; i < tableData.length - 1; i++) {
+        const d = new Date(tableData[i].date);
+        const fDate = `${d.getFullYear()} / ${(d.getMonth() + 1)
+            .toString()
+            .padStart(2, '0')} / ${d.getDate().toString().padStart(2, '0')}`;
+
+        const cases = tableData[i + 1].data.cases - tableData[i].data.cases;
+        const deaths = tableData[i + 1].data.deaths - tableData[i].data.deaths;
+
+        let table = document
+            .getElementById('corona-table')
+            .getElementsByTagName('tbody')[0];
+        let row = table.insertRow(0);
+
+        let c1 = row.insertCell(0);
+        let c2 = row.insertCell(1);
+        let c3 = row.insertCell(2);
+        let c4 = row.insertCell(3);
+
+        c1.innerHTML = escapeHTML(fDate);
+        c2.innerHTML = escapeHTML(
+            cases.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
+        );
+        c3.innerHTML = escapeHTML(
+            deaths.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
+        );
+        c4.innerHTML = escapeHTML(
+            tableData[i + 1].data.deaths
+                .toString()
+                .replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
+        );
+    }
+}
+
+function processData(data, tableData) {
+    Object.keys(data.timeline.cases).forEach((key) => {
+        tableData.push({
+            date: key,
+            data: { cases: data.timeline.cases[key] },
+        });
+    });
+
+    Object.keys(data.timeline.deaths).forEach((key) => {
+        const index = tableData.findIndex((item, i) => {
+            return item.date === key;
+        });
+        tableData[index].data.deaths = data.timeline.deaths[key];
+    });
+}
+
+function removeTablePlaceholder() {
+    document.getElementById('corona-table-placeholder').remove();
+}
+
 fetch('https://disease.sh/v3/covid-19/historical/hungary?lastdays=366').then(
     (resp) =>
         resp.json().then((data) => {
             let tableData = [];
 
-            Object.keys(data.timeline.cases).forEach((key) => {
-                tableData.push({
-                    date: key,
-                    data: { cases: data.timeline.cases[key] },
-                });
-            });
+            processData(data, tableData);
 
-            Object.keys(data.timeline.deaths).forEach((key) => {
-                const index = tableData.findIndex((item, i) => {
-                    return item.date === key;
-                });
-                tableData[index].data.deaths = data.timeline.deaths[key];
-            });
+            removeTablePlaceholder();
 
-            document.getElementById('corona-table-placeholder').remove();
-
-            for (let i = 0; i < tableData.length - 1; i++) {
-                const d = new Date(tableData[i].date);
-                const fDate = `${d.getFullYear()} / ${(d.getMonth() + 1)
-                    .toString()
-                    .padStart(2, '0')} / ${d
-                    .getDate()
-                    .toString()
-                    .padStart(2, '0')}`;
-
-                const cases =
-                    tableData[i + 1].data.cases - tableData[i].data.cases;
-                const deaths =
-                    tableData[i + 1].data.deaths - tableData[i].data.deaths;
-
-                let table = document
-                    .getElementById('corona-table')
-                    .getElementsByTagName('tbody')[0];
-                let row = table.insertRow(0);
-
-                let c1 = row.insertCell(0);
-                let c2 = row.insertCell(1);
-                let c3 = row.insertCell(2);
-                let c4 = row.insertCell(3);
-
-                c1.innerHTML = escapeHTML(fDate);
-                c2.innerHTML = escapeHTML(
-                    cases.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
-                );
-                c3.innerHTML = escapeHTML(
-                    deaths.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
-                );
-                c4.innerHTML = escapeHTML(
-                    tableData[i + 1].data.deaths
-                        .toString()
-                        .replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
-                );
-            }
+            addDataToTable(tableData);
         })
 );
